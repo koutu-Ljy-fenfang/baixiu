975eafc802fc3627fb7d3f710ad04c254d238401
itcast <itheima@itcast.cn>
2019-12-07 14:17:03 +0800

    文章分类全选功能

--- public/assets/js/category.js
+++ public/assets/js/category.js
@@ -84,3 +84,71 @@ $('#categoryBox').on('click', '.delete', function() {
 	}
 });
 
+
+// 获取全选按钮
+var selectAll = $('#selectAll');
+
+// 获取批量删除按钮
+var deleteMany = $('#deleteMany');
+
+// 当全选按钮的状态发生改变时
+selectAll.on('change', function () {
+	// 获取到全选按钮当前的状态
+	var status = $(this).prop('checked');
+
+	if (status) {
+		// 显示批量删除按钮
+		deleteMany.show();
+	}else {
+		// 隐藏批量删除按钮
+		deleteMany.hide();
+	}
+
+
+	// 获取到所有用户 并将用户的状态和全选按钮保持一致
+	$('#categoryBox').find('input').prop('checked', status);
+});
+
+$('#categoryBox').on('change', '.userStatus', function () {
+	// 获取到所有用户 在所有用户中过滤出选中的用户
+	// 判断选中用户的数量和所有用户的数量是否一致
+	// 如果一致 就说明所有的用户都是选中的
+	// 否则 就是有用户没有被选中
+	var inputs = $('#categoryBox').find('input');
+
+	if (inputs.length == inputs.filter(':checked').length) {
+		selectAll.prop('checked', true)
+	}else {
+		selectAll.prop('checked', false)
+	}
+
+	// 如果选中的复选框的数量大于0 就说明有选中的复选框
+	if (inputs.filter(':checked').length > 0) {
+		deleteMany.show();
+	}else {
+		deleteMany.hide();
+	}
+});
+
+// 为批量删除按钮添加点击事件
+deleteMany.on('click', function () {
+	var ids = [];
+	// 获取选中的用户
+	var checkedUser = $('#categoryBox').find('input').filter(':checked');
+	// 循环复选框 从复选框元素的身上获取data-id属性的值
+	checkedUser.each(function (index, element) {
+		ids.push($(element).attr('data-id'));
+	});
+	// console.log(ids);
+	
+	
+	if (confirm('您真要确定要进行批量删除操作吗')) {
+		$.ajax({
+			type: 'delete',
+			url: '/categories/' + ids.join('-'),
+			success: function () {
+				location.reload();
+			}
+		})
+	}
+});
