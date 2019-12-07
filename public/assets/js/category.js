147e75638fa5e18235b43ede9959930d1b465fbe
itcast <itheima@itcast.cn>
2019-12-07 09:22:10 +0800

    阿里百秀

---
+++ public/assets/js/category.js
@@ -0,0 +1,67 @@
+// 当添加分类表单发生提交行为的时候
+$('#addCategory').on('submit', function () {
+    // 获取用户在表单中输入的内容
+    var formData = $(this).serialize();
+
+    $.ajax({
+        type: 'POST',
+        url: '/categories',
+        data: formData,
+        success: function () {
+            location.reload();
+        }
+    })
+    // 阻止表单默认提交行为
+    return false;
+})
+
+// 发送ajax请求 向服务器端索要分类列表数据
+$.ajax({
+    type: 'GET',
+    url: '/categories',
+    success: function (response) {
+        var html = template('categoryListTpl', {
+            data: response
+        });
+        $('#categoryBox').html(html);
+    }
+});
+
+// 为编辑按钮添加点击事件
+$('#categoryBox').on('click', '.edit', function () {
+    // 获取用户在表单中输入的内容
+    var formData = $(this).serialize();
+    // 获取要修改的那个用户的id值
+    var id = $(this).attr('data-id');
+    // 发送请求 修改用户信息
+    $.ajax({
+        type: 'get',
+        url: '/categories/' + id,
+        success: function (response) {
+            console.log(response);
+            var html = template('modifyCategoryTpl', response);
+			$('#formBox').html(html);
+        }
+    })
+});
+
+// 为修改表单添加表单提交事件
+$('#formBox').on('submit', '#modifyCategory', function () {
+	// 获取用户在表单中输入的内容
+	var formData = $(this).serialize();
+	// 获取要修改的那个用户的id值
+	var id = $(this).attr('data-id');
+	// 发送请求 修改用户信息
+	$.ajax({
+		type: 'put',
+		url: '/categories/' + id,
+		data: formData,
+		success: function () {
+			// 修改用户信息成功 重新加载页面
+			location.reload()
+		}
+	})
+
+	// 阻止表单默认提交
+	return false;
+});
